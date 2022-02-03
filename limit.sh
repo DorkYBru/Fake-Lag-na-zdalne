5945d46f9358472258117c4aea07749175b003ec
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-02-03 20:08:33 +0100

    lepsza wersja

--- limit.sh
+++ limit.sh
@@ -1,25 +1,18 @@
+#!/bin/bash
+
+srututu=0
+read -p "napisz ile razy robic (w sekundach) " x
+while [ $srututu -le $x ];
+do
+slabe=(shuf -i 1-100000 -n 1)
 if [ -d wondershaper ]; then
-	wondershaper -a eno1 -d 128 -u 362
-	echo LIMITOWANIE_NETA
-	sleep 3
-	wondershaper -c -a eno1
-	wondershaper -a eno1 -d 2048 -u 2048
 	echo CHOKE
-	sleep 2
+	wondershaper -a eno1 -d $RANDOM -u $RANDOM
+	sleep 1
 	wondershaper -c -a eno1
-	wondershaper -a eno1 -d 22 -u 44
-	echo CHOKE
-	sleep 5
-	wondershaper -c -a eno1
-	wondershaper -a eno1 -d 2048 -u 2048
-	echo CHOKE
-	wondershaper -c -a eno1
-	wondershaper -a eno1 -d 128 -u 362
-	sleep 16
-	echo KONIEC
-	wondershaper -c -a eno1
+	srututu=$(( $srututu + 1 ))
 else
 	wget "https://raw.githubusercontent.com/DorkYBru/Limiter-Neta-na-zdalne/main/installwondershaper.sh"
 	bash installwondershaper.sh.1
-	echo "jeszcze raz włącz skrypt";
-	fi
+		fi
+		done
