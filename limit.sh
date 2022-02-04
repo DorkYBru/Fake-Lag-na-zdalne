60a1cc91961ca2af50f9de55d58f5a6260583a26
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-02-04 10:29:41 +0100

    tadeusz

--- limit.sh
+++ limit.sh
@@ -4,10 +4,11 @@ srututu=0
 read -p "napisz ile razy robic (w sekundach) " x
 while [ $srututu -le $x ];
 do
-slabe=(shuf -i 1-100000 -n 1)
+slabe="${RANDOM:0:2}"
 if [ -d wondershaper ]; then
 	echo CHOKE
-	wondershaper -a eno1 -d $RANDOM -u $RANDOM
+	echo $slabe
+	wondershaper -a eno1 -d $slabe -u $slabe
 	sleep 1
 	wondershaper -c -a eno1
 	srututu=$(( $srututu + 1 ))
